9b55754478437b8e0a678d29c6ddfdb35d821a1d
andreespirela <andreespirela@outlook.com>
2024-07-23 12:48:43 -0400

    chore: Modify irys requests

--- wvm-apps/wvm-exexed/crates/reth-exexed/src/main.rs
+++ wvm-apps/wvm-exexed/crates/reth-exexed/src/main.rs
@@ -6,6 +6,7 @@ pub mod util;
 
 use crate::util::to_brotli;
 use bigquery::client::BigQueryConfig;
+use irys::irys::IrysRequest;
 use lambda::lambda::exex_lambda_processor;
 use repository::state_repository;
 use reth::api::FullNodeComponents;
@@ -46,13 +47,19 @@ async fn exex_etl_processor<Node: FullNodeComponents>(
             let brotli_borsh = to_brotli(borsh_data);
             let json_str = to_string(&sealed_block_with_senders)?;
 
-            let arweave_id = irys_provider.upload_data_to_irys(brotli_borsh).await?;
+            let arweave_id = IrysRequest::new()
+                .set_tag("Content-Type", "application/octet-stream")
+                .set_tag("WeaveVM:Encoding", "Borsh,Brotli")
+                .set_data(brotli_borsh)
+                .send_with_provider(&irys_provider)
+                .await?;
+
             println!("irys id: {}", arweave_id);
 
             state_repository
                 .save(ExecutionTipState {
                     block_number: committed_chain.tip().block.number,
-                    arweave_id,
+                    arweave_id: arweave_id.clone(),
                     sealed_block_with_senders_serialized: json_str,
                 })
                 .await?;
