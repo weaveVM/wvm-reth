3d156e467dcadd39fee2e23d6c2b11d91df420bd
andreespirela <andreespirela@outlook.com>
2024-10-01 13:13:02 -0400

    chore: WVM

--- crates/chainspec/src/constants.rs
+++ crates/chainspec/src/constants.rs
@@ -2,7 +2,7 @@ use crate::spec::DepositContract;
 use alloy_primitives::{address, b256};
 
 /// Gas per transaction not creating a contract.
-/// Wevm: Raised from 21k to 500_000k
+/// WVM: Raised from 21k to 500_000k
 pub const MIN_TRANSACTION_GAS: u64 = 500_000u64;
 /// Deposit contract address: `0x00000000219ab540356cbb839cbe05303d7705fa`
 pub(crate) const MAINNET_DEPOSIT_CONTRACT: DepositContract = DepositContract::new(
